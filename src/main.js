93d143a5815d88e0143a914e5b8d148b1a1c9e7f
Jhonny Córdova <jcordova@borealis.cl>
2018-10-13 19:21:40 -0300

    Filtros

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from '@/App.vue'
 import EventBus from '@/plugins/event-bus'
+import msToMm from '@/filters/ms-to-mm'
 
 import routes from '@/routes'
 
 Vue.use(EventBus)
 Vue.use(VueRouter)
+Vue.use(msToMm)
 
 const router = new VueRouter({ routes, mode: 'history' })
 
