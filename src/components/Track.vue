2007101217206e570903f22ca5ffb24b0f78bfaf
Jhonny Córdova <jcordova@borealis.cl>
2018-10-14 08:57:11 -0300

    mixins

--- src/components/Track.vue
+++ src/components/Track.vue
@@ -25,18 +25,15 @@
 </template>
 
 <script>
+import trackMixin from '@/mixins/track'
+
 export default {
+  mixins: [trackMixin],
   props: {
     track: { type: Object, required: true }
   },
 
   methods: {
-    selectTrack () {
-      if (!this.track.preview_url) { return }
-      this.$emit('select', this.track.id)
-      this.$bus.$emit('set-track', this.track) // Emito en otra instancia de Vue (la creada en el plugin) para que componentes no relacionados compartan informacion
-    },
-
     goToTrack (id) {
       if (!this.track.preview_url) { return }
       this.$router.push({ name: 'track', params: { id: id } })
